ac826f1cd1f9b10a546ab7dbd16aa7613365ce6b
KaoZCTM <ce.gangas@duocuc.cl>
2025-03-28 20:35:39 -0300

    yaml fix2

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,3 @@ Route::get('/', function () {
     return view('welcome'); // Asegúrate de tener resources/views/welcome.blade.php
 })->name('home');
 
-Route::get('/debug', function() {
-    \Log::error('Prueba de log desde Laravel');
-    return phpinfo();
-});
